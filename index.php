191e2ec432c6f7b9356f5fd5990cc1d46032b8ea
Michael Hoffmann <michi@hey.com>
2024-03-14 02:11:28 +0100

    Update SDK

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ require 'vendor/autoload.php';
 
 \Sentry\init([
     'dsn' => 'https://3a00fc26ae8bb0e0962c4e7ed95dc933@o711518.ingest.us.sentry.io/4506906351828992',
+    'release' => trim(exec('git describe --tags --abbrev=0')),
 ]);
 
-ExceptionService::throw('Oh no');
+ExceptionService::throw('Oh no!');
+ExceptionService::throw('Oh no, again?');
